61b6f4bdd4b436995f71509b81523c7fcf1f27aa
Hirota,Shinnosuke S <s.hirota@student.fontys.nl>
2018-09-23 21:42:29 +0200

    implement Dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { DepartmentsComponent } from './departments/departments.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { TasksComponent } from './tasks/tasks.component';
+
+const routes:Routes =[
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'departments', component: DepartmentsComponent},
+  { path: 'employees', component: EmployeesComponent},
+  { path: 'tasks', component: TasksComponent},
+];
 
 @NgModule({
-   exports: [ RouterModule ]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
-
-const rutes:Routes =[
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent }
-];
